cf18c2edf4517e9504d16b32b72c5399cbadda49
Farid AMOUSSA <amousssa.farid2@gmail.com>
2021-07-09 08:57:54 +0100

    Create new task creation screen and edit screen

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+    before_action :set_task, only: [:edit, :update]
+    def index
+      @tasks = Task.all
+    end
+  
+    def new
+      @task = Task.new
+    end
+  
+    def create
+        @task = Task.new(task_params)
+        if @task.save
+            redirect_to tasks_path, notice:"Task create successfully"
+        else
+            render :new
+        end
+    end
+  
+    def edit
+    end
+  
+    def update
+        if @task.update(task_params)
+            redirect_to tasks_path, notice:"Task updated successfully"
+        else
+            render :new
+        end
+    end
+  
+    def destroy
+    end
+  
+    private
+    def set_task
+      @task = Task.find(params[:id])
+    end
+    
+    def task_params
+        params.require(:task).permit(:name,:details)
+    end
+    
+  end
+  
